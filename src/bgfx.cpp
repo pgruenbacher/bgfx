7bca3fd383961c071b1cea38cf49c44de000b45b
paul gruenbacher <pgruenbacher@gmail.com>
2022-01-18 23:48:04 -0600

    handle vk uniforms and hide shaderc spirv output trace spam

--- src/bgfx.cpp
+++ src/bgfx.cpp
@@ -9,7 +9,6 @@
 #include <bgfx/embedded_shader.h>
 #include <bx/file.h>
 #include <bx/mutex.h>
-
 #include "topology.h"
 
 #if BX_PLATFORM_OSX || BX_PLATFORM_IOS || BX_PLATFORM_VISIONOS
@@ -2706,6 +2705,7 @@ namespace bgfx
 					score += 1000;
 				}
 
+
 				score += RendererType::Noop != renderer ? 1 : 0;
 
 				if (BX_ENABLED(BX_PLATFORM_WINDOWS) )
@@ -2781,6 +2781,7 @@ namespace bgfx
 
 			s_rendererCreator[renderer].supported = false;
 		}
+
 
 		return renderCtx;
 	}
@@ -4785,6 +4786,8 @@ namespace bgfx
 		BGFX_ERROR_CHECK(false
 			|| bx::isAlpha(ch)
 			|| '_' == ch
+			|| '@' == ch
+			|| '.' == ch
 			, _err
 			, BGFX_ERROR_IDENTIFIER_VALIDATION
 			, "The first character of an identifier should be either an alphabet character or an underscore."
@@ -4798,7 +4801,7 @@ namespace bgfx
 			; ++ptr
 			)
 		{
-			result &= bx::isAlphaNum(*ptr) || '_' == *ptr;
+			result &= bx::isAlphaNum(*ptr) || '_' == *ptr || '@' == *ptr || '.' == *ptr;
 		}
 
 		BGFX_ERROR_CHECK(false
