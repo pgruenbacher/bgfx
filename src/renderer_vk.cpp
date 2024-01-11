1af30f7751e8bf0a1ad02f8852f14b9ec50df976
paul gruenbacher <pgruenbacher@gmail.com>
2024-01-11 13:17:23 -0600

    avoid renderdoc for now

--- src/renderer_vk.cpp
+++ src/renderer_vk.cpp
@@ -1211,11 +1211,12 @@ VK_IMPORT_DEVICE
 			VkResult result;
 			m_globalQueueFamily = UINT32_MAX;
 
-			if (_init.debug
-			||  _init.profile)
-			{
-				m_renderDocDll = loadRenderDoc();
-			}
+			// disabled by paul to avoid vulkan layer statistics always showing annoyingly
+			// if (_init.debug
+			// ||  _init.profile)
+			// {
+			// 	m_renderDocDll = loadRenderDoc();
+			// }
 
 			setGraphicsDebuggerPresent(false
 				|| NULL != m_renderDocDll
